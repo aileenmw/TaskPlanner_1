9437eb9b0f12f07be08985cdc6656147686afd5f
aileen westphal <aileenmw@gmail.com>
2022-01-21 17:39:06 +0100

    Add project files.

---
+++ TaskPlanner/Data/Migrations/20220119175209_InitialMigration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskPlanner.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "VARCHAR(100)", maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "VARCHAR(100)", maxLength: 100, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Worker = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "VARCHAR(100)", maxLength: 100, nullable: false),
+                    MinAge = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ TaskPlanner/SqlServer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.Helpers
+{
+    public class SqlServer
+    {
+        // Enter Your SQL Server name here:
+        private static string YourSqlServer = "";
+
+        private static string serverName = YourSqlServer != "" ? YourSqlServer : System.Environment.MachineName;
+        public static string Connection = $"Server={serverName};Database=TaskPlanner;Trusted_Connection=True;MultipleActiveResultSets=True";
+        public bool IsServerConnected(string connectionString)
+        {
+            bool con = false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    con = true;
+                }
+                catch (SqlException)
+                {
+                    con = false;
+                }
+                
+                finally
+                {
+                    try
+                    {
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                
+            }
+            return con;
+        }
+
+        public void setConnection(string connectionString)
+        {
+            bool con = false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    con = true;
+                }
+                catch (SqlException)
+                {
+                    con = false;
+                }
+            }
+        }
+             
+    }
+}
